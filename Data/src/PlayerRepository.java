5d4f24a044c3b73000c80596b6560d1a88480920
md@id.uff.br <md@id.uff.br>
2014-11-13 23:00:15 -0200

    Corrigindo o PlayerRepository

--- Data/src/PlayerRepository.java
+++ Data/src/PlayerRepository.java
@@ -3,118 +3,111 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-
-public class PlayerRepository implements IPlayerRepository
-{
+public class PlayerRepository implements IPlayerRepository {
 	private HashMap<String, Player> players;
 	private IPlanetRepository planetRepository;
-	public PlayerRepository()
-	{
+
+	public PlayerRepository() {
 		players = new HashMap<String, Player>();
 	}
-	
-	public List<Player> getAllPlayers()
-	{
+
+	public List<Player> getAllPlayers() {
 		List<Player> playersList = new ArrayList<Player>();
 		Player player;
-		for(String playerName : players.keySet())
-		{
+		for (String playerName : players.keySet()) {
 			player = getPlayerByName(playerName);
 			playersList.add(player);
 		}
-			
+
 		return playersList;
 	}
-	
-	public List<Piece> getAllPieces() 
-	{
+
+	public List<Piece> getAllPieces() {
 		ArrayList<Piece> pieces = new ArrayList<Piece>();
 		for (Player player : players.values())
 			for (Piece piece : player.getPieces())
 				pieces.add(piece);
 		return pieces;
 	}
-	
-	public Piece getPiece(int pieceId) 
-	{
+
+	public Piece getPiece(int pieceId) {
 		for (Player player : players.values())
 			for (Piece piece : player.getPieces())
 				if (piece.getId() == pieceId)
 					return piece;
 		return null;
 	}
-	
-	public void addPlayer(Player player)
-	{
+
+	public void addPlayer(Player player) {
 		players.put(player.getName(), player);
 	}
-	
-	public Player getPlayerByName(String name) 
-	{
+
+	public Player getPlayerByName(String name) {
 		if (name == null || name.isEmpty())
 			return null;
 		return players.get(name);
 	}
-		
-	public void addPlayerPiece(String playerName, String planetName, int numberOfPieces)
-	{
-		for (int i = 0 ; i < numberOfPieces; i++)
-		{
+
+	public void addPlayerPiece(String playerName, String planetName,
+			int numberOfPieces) {
+		for (int i = 0; i < numberOfPieces; i++) {
 			Player player = getPlayerByName(playerName);
 			Piece piece = new Piece();
 			if (piece.getId() == 0)
 				piece.setId(RandomNumberGenerator.generateRandomNumber());
 			piece.setTerritoryName(planetName);
-			
-			switch (player.getRace())
-			{
-				case PADAWAN:
-					piece.setImagePath("image/padawan.png");
-					piece.setPieceType(PieceType.JEDI);
-					break;
-				case TRANDOSHAN:
-					piece.setImagePath("image/trandoshan.png");
-					piece.setPieceType(PieceType.TRANDOSHAN);
-					break;
-				case SITH:
-					piece.setImagePath("image/sithtrooper.png");
-					piece.setPieceType(PieceType.SITH);
-					break;
-				case CLONE:
-					piece.setImagePath("image/clone.png");
-					piece.setPieceType(PieceType.CLONE);
-					break;
-				case DROIDE:
-					piece.setImagePath("image/droide.png");
-					piece.setPieceType(PieceType.DROIDE);
-					break;
-				case WOOKIEE:
-					piece.setImagePath("image/wookiee.png");
-					piece.setPieceType(PieceType.WOOKIEE);
-					break;
+
+			switch (player.getRace()) {
+			case PADAWAN:
+				piece.setImagePath("image/padawan.png");
+				piece.setPieceType(PieceType.JEDI);
+				break;
+			case TRANDOSHAN:
+				piece.setImagePath("image/trandoshan.png");
+				piece.setPieceType(PieceType.TRANDOSHAN);
+				break;
+			case SITH:
+				piece.setImagePath("image/sithtrooper.png");
+				piece.setPieceType(PieceType.SITH);
+				break;
+			case CLONE:
+				piece.setImagePath("image/clone.png");
+				piece.setPieceType(PieceType.CLONE);
+				break;
+			case DROIDE:
+				piece.setImagePath("image/droide.png");
+				piece.setPieceType(PieceType.DROIDE);
+				break;
+			case WOOKIEE:
+				piece.setImagePath("image/wookiee.png");
+				piece.setPieceType(PieceType.WOOKIEE);
+				break;
 			}
-			planetRepository.getPlanetByName(planetName).setOwnerName(playerName);
-			piece.setXAxisCoordinate(planetRepository.getPlanetByName(planetName).getXAxisCoordinate());
-			piece.setYAxisCoordinate(planetRepository.getPlanetByName(planetName).getYAxisCoordinate());
+			planetRepository.getPlanetByName(planetName).setOwnerName(
+					playerName);
+			piece.setXAxisCoordinate(planetRepository.getPlanetByName(
+					planetName).getXAxisCoordinate());
+			piece.setYAxisCoordinate(planetRepository.getPlanetByName(
+					planetName).getYAxisCoordinate());
 			player.addPiece(piece);
-		}		
-	}
-	
-	public void removePlayerPiece(String playerName, String planetName, int numberOfPieces)
-	{
-		int numberOfRemovedPieces = 0;
-		Player player = getPlayerByName(playerName);
-		player.getPieces();
-		while(player.getPieces().iterator().hasNext() && numberOfPieces > numberOfRemovedPieces ){
-			Piece piece = player.getPieces().iterator().next();
-			if(piece.getTerritoryName().equals(planetName)){
-					player.getPieces().remove(piece);
-					numberOfRemovedPieces++;
-			}
 		}
 	}
-	
-	public void restartRepository(){
+
+	public void removePlayerPiece(String playerName, String planetName,
+			int numberOfPieces) {
+		Player player = getPlayerByName(playerName);
+		Iterator<Piece> pieceIterator = player.getPieces().iterator();
+		while (numberOfPieces > 0 && pieceIterator.hasNext()) {
+			Piece piece = pieceIterator.next();
+			if (piece.getTerritoryName().equalsIgnoreCase(planetName)) {
+				player.getPieces().remove(piece);
+				numberOfPieces--;
+			}
+
+		}
+	}
+
+	public void restartRepository() {
 		players = new HashMap<String, Player>();
 	}
 
@@ -125,5 +118,5 @@ public class PlayerRepository implements IPlayerRepository
 	public void setPlanetRepository(IPlanetRepository planetRepository) {
 		this.planetRepository = planetRepository;
 	}
-	
+
 }
